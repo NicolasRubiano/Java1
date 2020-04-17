1f118c290018a1ee0fd5bc50068e1b6f924f220c
nicolas <nrubiano12@unisalle.edu.co>
2020-04-17 14:27:55 -0500

    Interfaz vd 124

---
+++ src/practica1/AplicacionDialog.java
@@ -0,0 +1,13 @@
+package practica1;
+
+import javax.swing.JFrame;
+
+public class AplicacionDialog {
+
+	public static void main(String[] args) {
+	MarcoDialogos mimarco=new MarcoDialogos();
+	mimarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	mimarco.setVisible(true);
+	}
+
+}
---
+++ src/practica1/MarcoDialogos.java
@@ -0,0 +1,23 @@
+package practica1;
+import java.awt.GridLayout;
+
+import javax.swing.*;
+
+public class MarcoDialogos extends JFrame{
+private LaminaBotones lamina_tipo;
+	public MarcoDialogos() {
+		setTitle("Prueba de diálogos");
+		setBounds(500,200,600,450);
+		JPanel lamina_cuadricula=new JPanel();//Agregar lamina tipo float
+		lamina_cuadricula.setLayout(new GridLayout(2,3));//Cambiar lamina a tipo gridLayout
+		add(lamina_cuadricula);//Mostrar lamina
+		String primero[]= {"Mensaje", "Confirmar","Opción","Entrada"};
+		lamina_tipo=new LaminaBotones("Tipo", primero);
+		lamina_cuadricula.add(lamina_tipo);
+		add(lamina_cuadricula);
+		}
+	
+	
+	
+	
+}
